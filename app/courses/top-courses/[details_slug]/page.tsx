f396c5924c91179c17dcea2fc59b76fe30fd96da
emdadul7171 <emdadul.m4yours@gmail.com>
2024-02-07 11:25:00 +0600

    fix

--- app/courses/top-courses/[details_slug]/page.tsx
+++ app/courses/top-courses/[details_slug]/page.tsx
@@ -6,30 +6,30 @@ import { notFound } from "next/navigation";
 import TopCoursesDetails from "@/modules/courses/top-courses/details";
 
 async function getData(params: any) {
-  const filePath = path.join(
-    process.cwd(),
-    "/data/top-courses-single-data.json"
-  );
+  try {
+    const filePath = path.join(
+      process.cwd(),
+      "/data/top-courses-single-data.json"
+    );
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData.toString());
 
-  const jsonData = await fsPromises.readFile(filePath);
-  const objectData = JSON.parse(jsonData.toString());
+    const res = objectData.data?.find(
+      (item: { slug: string }) => item.slug === params.details_slug
+    );
 
-  const res = objectData.data?.find(
-    (item: { slug: string }) => item.slug === params.details_slug
-  );
+    if (!res) {
+      throw new Error("Faild to Fetch Data");
+    }
 
-  if (!res) {
-    return notFound();
-  } else {
     return res;
+  } catch (error) {
+    return notFound();
   }
 }
 
 const TopCoursesDetailsPage = async ({ params }: any) => {
   const data = await getData(params);
-
-  console.log("params", params);
-  console.log("data", data);
 
   return <TopCoursesDetails data={data} params={params?.details_slug} />;
 };
